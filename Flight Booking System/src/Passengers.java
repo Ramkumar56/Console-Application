003486f3204ac24f73287590c199447a3d78be21
Ramkumar Dhamodharan <64086675+Ramkumar56@users.noreply.github.com>
2022-09-29 23:15:55 +0530

    Add files via upload

---
+++ Flight Booking System/src/Passengers.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Passengers {
+    String Name;
+    int PassportNumber;
+    List<Passengers> PassengersDetails;
+    Passengers(){
+        PassengersDetails=new ArrayList<>();
+    }
+}
